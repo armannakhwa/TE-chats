93c0fb56bea21ce6cc9b0ffd2f56dcb35f6ea6e3
Arman Nakhwa <armannakhwa19@gmail.com>
2021-10-14 17:15:07 +0530

    te group chat app first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const cors = require('cors');
+var express = require('express')
+var app = express()
+
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+app.use(cors())
+let users = {}
+
+
+io.on("connection", socket => {
+
+    socket.on("users", name => {
+        users[socket.id] = name;
+        console.log(users[socket.id]);
+        socket.broadcast.emit("user", name);
+    })
+
+    socket.on("new-msg", ({ msg: msg, name: name }) => {
+        socket.broadcast.emit("msgs", { msg, name });
+    })
+
+    socket.on("disconnect", name => {
+        socket.broadcast.emit("leave", users[socket.id]);
+        delete users[socket.id];
+    })
+
+})
+
+
+app.get('/', function(req, res,next) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(process.env.PORT || 30);
