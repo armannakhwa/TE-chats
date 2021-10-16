c8ebd833adfb5d5ec4d9f5d2e02a9873c8676b51
Arman Nakhwa <armannakhwa19@gmail.com>
2021-10-16 20:20:29 +0530

    online user tab added

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
-const cors = require('cors');
-var express = require('express')
-var app = express()
+const cors = require("cors");
+var express = require("express");
+var app = express();
 
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.use(cors())
-let users = {}
+app.use(cors());
+let users = {};
 
-
-io.on("connection", socket => {
-
-    socket.on("users", name => {
+io.on("connection", (socket) => {
+    socket.on("users", (name) => {
         users[socket.id] = name;
         console.log(users[socket.id]);
         socket.broadcast.emit("user", name);
-    })
+        io.emit("allusers", users);
+    });
 
     socket.on("new-msg", ({ msg: msg, name: name }) => {
         socket.broadcast.emit("msgs", { msg, name });
-    })
+    });
 
-    socket.on("disconnect", name => {
+    socket.on("disconnect", (name) => {
         socket.broadcast.emit("leave", users[socket.id]);
         delete users[socket.id];
-    })
+        io.emit("allusers", users);
+    });
+});
 
-})
-
-
-app.get('/', function(req, res,next) {
-    res.sendFile(__dirname + '/index.html');
+app.get("/", function (req, res, next) {
+    res.sendFile(__dirname + "/index.html");
 });
 
 server.listen(process.env.PORT || 30);
